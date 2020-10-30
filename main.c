f02a3212c9be9ef48a4ebed961a94105c33a33f6
์Nar'nan <3bc16017@gmail.com>
2020-10-30 09:48:36 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+  int  a, b,c,d,i;
+  for (int i= 0;i<= 9999;i++) {
+		a = i/1000;
+		b = (i%1000)/100;
+		c = (i%100)/10;
+		d = i%10;
+	  if ((a, 4) +(b, 4) + (c, 4) + (d, 4) == i );
+            continue;
+        printf(a + "^4 +", + b +"^4 +", + c + "^4 + ", + d + "^4 =", + i);
+        
+    }
+    return 0;
+}
